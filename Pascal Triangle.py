aa6a3bfd37361590279b189113ecbb9542667e00
thewholebowl <agarwalgarv.2311@gmail.com>
2026-01-02 19:42:55 +0530

    Refactor Pascal Triangle code into a function
    I mean it looks better this way

--- Pascal Triangle.py
+++ Pascal Triangle.py
@@ -1,11 +1,13 @@
-from math import *
-x = int(input("Enter number of rows of Pascal's Triangle: "))
-for i in range(1,x+1,1):
-    for j in range(x-i,-1,-1):
-        print("   ",end="")
-    for k in range(1,i+1,1):
-        r = factorial(i-1)
-        e = factorial(k-1)
-        d = factorial(i-k)
-        print(int(r/(e*d)),end="   ")
-    print()
+from math import factorial
+def pascal(x):
+    for i in range(1,x+1,1):
+        for j in range(x-i,-1,-1):
+            print("   ",end="")
+        for k in range(1,i+1,1):
+            r = factorial(i-1)
+            e = factorial(k-1)
+            d = factorial(i-k)
+            print(int(r/(e*d)),end="   ")
+        print()
+x = int(input("Enter number of rows of pascal's triangle: "))
+pascal(x)
