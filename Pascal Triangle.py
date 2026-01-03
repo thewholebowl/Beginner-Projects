2fe9c6c7be56abdf30ff63405c10287f5c0feecd
thewholebowl <agarwalgarv.2311@gmail.com>
2026-01-03 21:35:02 +0530

    Made rows of Pascal's Triangle uniform
    This was my not optimum but works approach to the pascal's triangle

--- Pascal Triangle.py
+++ Pascal Triangle.py
@@ -2,12 +2,15 @@ from math import factorial
 def pascal(x):
     for i in range(1,x+1,1):
         for j in range(x-i,-1,-1):
-            print("   ",end="")
+            print("      ",end="")
         for k in range(1,i+1,1):
             r = factorial(i-1)
             e = factorial(k-1)
             d = factorial(i-k)
-            print(int(r/(e*d)),end="   ")
+            print(int(r/(e*d)),end="")
+            for t in range(10-(len(str(int(r/(e*d)))))):
+                print(" ",end="")
         print()
-x = int(input("Enter number of rows of pascal's triangle: "))
-pascal(x)
+while True:
+    x = int(input("Enter number of rows of pascal's triangle: "))
+    pascal(x)
