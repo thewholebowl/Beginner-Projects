eea2dc38287b08e3fdfe97022ee8ce6ea16e761b
thewholebowl <agarwalgarv.2311@gmail.com>
2026-01-09 11:30:04 +0530

    Implement Collatz sequence calculation

---
+++ collatz sequence.py
@@ -0,0 +1,9 @@
+x = int(input("Enter number to find collatz sequence: "))
+print(x)
+while x != 1:
+    if x % 2 == 0:
+        x = x // 2
+    else:
+        x = 3 * x + 1
+    print(x)
+print("Sequence entered loop")
