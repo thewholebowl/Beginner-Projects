6a68530a2729065909433755a96909fb5a109747
thewholebowl <agarwalgarv.2311@gmail.com>
2026-01-09 11:02:48 +0530

    Refactor prime generation logic for efficiency
    Replaced the prime checking method with a simpler approach using division by 2 instead of square root.

--- prime generator.py
+++ prime generator.py
@@ -1,9 +1,8 @@
-from math import *
 def primes(t):
     l = []
     for i in range(2,t+1,1):
         prim = True
-        for j in range(2,round(sqrt(i)),1):
+        for j in range(2,(i//2)+1,1):      #I did not use sqrt() because it causes errors in the first few numbers
             if i%j==0:
                 prim = False
                 break
@@ -14,3 +13,4 @@ def primes(t):
     print(l)
 s = int(input("Enter upper limit for the primes: "))
 primes(s)
+
